4ee7d222bba24ff3192e0bdde5c6a5335bb1fbcd
hikammuzakky <hikammuzakky@gmail.com>
2024-10-01 10:57:43 +0700

    try convert.py

---
+++ convert.py
@@ -0,0 +1,11 @@
+import os
+
+# read mp3 files from audio directory and list them
+def read_mp3_files():
+    audio_files = []
+    for file in os.listdir('audio'):
+        if file.endswith('.mp3'):
+            audio_files.append(file)
+    return audio_files
+
+print(read_mp3_files())
